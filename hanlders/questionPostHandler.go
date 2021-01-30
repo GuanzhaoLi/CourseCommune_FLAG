89dc92cf40df813cbd641f6506bc90af9bd09da9
bubingbing <73148185+bubingbing@users.noreply.github.com>
2021-01-30 04:05:37 -0500

    Update questionPostHandler.go

--- hanlders/questionPostHandler.go
+++ hanlders/questionPostHandler.go
@@ -1,22 +1,85 @@
 package main
 
-import {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"regexp"
 
-}
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
 
 // receive JSON, return ?
 func questionPostHandler(w http.ResponseWriter, r *http.Request) {
-	//get the question
-	q := Question{
+	//setting:
+	fmt.Println("Received one request for upload")
+	w.Header().Set("Content-Type", "application/json") // return type is json
+	//cross domain access:option
+	w.Header().Set("Access-Control-Allow-Origin", "*")                           // can be accessed by any domain
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization") //back end allow http header which is content -type and Authorization
+	// if request is options do nothing just return ok
+	if r.Method == "OPTIONS" {
+		return
+	}
 
+	//get user's information from body
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
+
+	q := Post{
+		User:    username.(string),
+		Message: r.FormValue("message"),
 	}
 
 	// post the question
-	err := saveQuestionPost(&q)
+	err := saveQuestionPost(&p)
 	if err != nil {
-
+		http.Error(w, "Failed to post question", http.StatusInternalServerError)
+		return
 	}
+	fmt.Println("Post is saved successfully")
 }
+
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	w.Header().Set("Content-Type", "application/json")
+	//cross domain access:option
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+	keywords := r.URL.Query().Get("keywords")
+
+	var posts []Post
+	var err error
+	posts, err = searchPostsByKeywords(keywords)
+
+	if err != nil {
+		http.Error(w, "Failed to read post from Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from Elasticsearch %v.\n", err)
+		return
+	}
+
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
+	w.Write(js)
+}
+
+
+
+
 
 func saveQuestionPost(question *Question) error {
 	// create database client
