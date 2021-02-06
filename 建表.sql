0f625feba94448079fd609e4fe3658fe6b3672b1
GoldenWingman <40277753+GuanzhaoLi@users.noreply.github.com>
2021-02-05 21:30:31 -0500

    Add files via upload

--- 建表.sql
+++ 建表.sql
@@ -6,7 +6,7 @@ create table VideoOrder(OrderId int, \
 	Subject varchar(50), Level int, Keywords varchar(256), \
 	Tutor_Student_R int, 
 	Student_Tutor_R int, 
-	Primary Key(OrderId));
+	Primary Key(OrderId))ENGINE = MYISAM;
 
 
 
@@ -16,7 +16,7 @@ create table QuestionOrder(
 	RequestBy int, FulfilledBy int,
 	Subject varchar(50), Level int, Keywords varchar(256), \
 	Student_Tutor_R int, Primary Key(QId)
-	);
+	)ENGINE = MYISAM;
 
 
 create table Tutor(
@@ -27,7 +27,7 @@ create table Tutor(
 	Account_Balance int,
 	Rating int,
 	Primary Key(TutorId)
-);
+)ENGINE = MYISAM;
 
 create table Student(
 	StudentId int, 
@@ -37,18 +37,18 @@ create table Student(
 	Account_Balance int,
 	Rating int,
 	Primary Key(StudentId)
-);
+)ENGINE = MYISAM;
 
 create table Users(
 	Id int,
 	email varchar(256),
 	password varchar(256),
 	birthday date,
-	StudentId,
-	TutorId,
-	Foreign Key(StudentId) References Student(StudentId) ON DELETE CASCADE ON Update CASCADE
+	StudentId int,
+	TutorId int,
+	Foreign Key(StudentId) References Student(StudentId) ON DELETE CASCADE ON Update CASCADE,
 	Foreign Key(TutorId) References Tutor(TutorId) ON DELETE CASCADE ON Update CASCADE
-)
+)ENGINE = MYISAM;
 
 
 create table Student_Preference(
@@ -56,7 +56,7 @@ create table Student_Preference(
 	Prefered_tutor int,
 	Comments varchar(256),
 	Foreign Key(id) References Student(StudentId) ON DELETE CASCADE ON Update CASCADE
-);
+)ENGINE = MYISAM;
 
 
 create table Student_History(
@@ -64,7 +64,7 @@ create table Student_History(
 	VideoOrder int,
 	QuestionOrder int,
 	Foreign Key(id) References Student(StudentId) ON DELETE CASCADE ON Update CASCADE
-);
+)ENGINE = MYISAM;
 
 
 create table Tutor_History(
@@ -72,4 +72,4 @@ create table Tutor_History(
 	VideoOrder int,
 	QuestionOrder int,
 	Foreign Key(id) References Tutor(TutorId) ON DELETE CASCADE ON Update CASCADE
-);
+)ENGINE = MYISAM;
