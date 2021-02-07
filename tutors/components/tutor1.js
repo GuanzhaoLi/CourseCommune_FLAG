bd70b1b75ee16beec2cb59cfb2d3a1336d5492dd
Luccascis <76143354+Luccascis@users.noreply.github.com>
2021-02-08 00:24:57 +0800

    Update tutor1.js

--- tutors/components/tutor1.js
+++ tutors/components/tutor1.js
@@ -4,18 +4,13 @@ import imagesloaded  from 'imagesloaded' //监听图片加载
 import InfiniteScroll from 'react-infinite-scroller' //下拉加载
 import axios from 'axios'
 
-import {Button, Card, Modal} from 'antd'
-
-
-
-// get data here
-// test data
+import {Card} from 'antd'
+import VideoDetails from './videoDetails'
 
 export default class Pinterest extends Component {
     state = {
         data: [],
         hasMore: true,
-        isModalVisible:false
     }
 
     getVideoData = ()=> {
@@ -38,44 +33,15 @@ export default class Pinterest extends Component {
         this.imagesOnload()
     }
 
-    showModal = () => {
+    update = (newData) => {
+        const data = this.state.data
         this.setState({
-            isModalVisible : true
-        })
-    };
-
-    handleCancel = () => {
-        this.setState({
-            isModalVisible : false
-        })
-    };
-
-    accept = () =>{
-        // when tutor accept to get the video request, click ok
-        //update the information here
-
-        const opt = {
-            method: "POST",
-            url: `/accept`,
-            data: {
-                acceptOrNot: true
-            },
-        }
-        axios(opt)
-            .then((res) => {
-                if (res.status === 200) {
-                    console.log(res.data);
+            data: data.map((item)=>{
+                if(item.id===newData.id){
+                    item.accepted = newData.accepted
                 }
+                return item
             })
-            .catch((err) => {
-                console.log("答案上传错误 ", err.message);
-                alert("Error happens, Please update the answer again!");
-            });
-
-
-        alert("已接受预约");
-        this.setState({
-            isModalVisible : false
         })
     }
 
@@ -106,7 +72,6 @@ export default class Pinterest extends Component {
         let data = Array.from(this.state.data);
         return (
             <div className='pages_pinterest'>
-                {/* 下拉加载 */}
                 <InfiniteScroll
                     initialLoad={false}
                     hasMore={hasMore}
@@ -117,25 +82,14 @@ export default class Pinterest extends Component {
                             data.map((item, index) => {
                                     return (
                                         <div key={index} className='imgBox'>
-                                            <Card className="cards">
+                                            <Card className="cards" actions={[<VideoDetails update={this.update} data={item}></VideoDetails>]}>
                                                 <p>视频任务{index}</p>
                                                 <p>日期：{item.startDate}-{item.endDate}</p>
                                                 <p>学生：{item.avatar}</p>
                                                 <p>学科: {item.subject}</p>
                                                 <p>描述: {item.content}</p>
-                                                <Button type="primary" onClick={this.showModal}>
-                                                    预约
-                                                </Button>
+                                                <p style={{color: 'red',textAlign:'center'}}>{item.accepted? "已预约":" "}</p>
                                             </Card>
-                                            <Modal width={1000} title="问题详情" visible={this.state.isModalVisible}
-                                                   onOk={this.accept} onCancel={this.handleCancel}>
-                                                <p>视频任务{index}</p>
-                                                <p>日期：{item.startDate}-{item.endDate}</p>
-                                                <p>学生：{item.avatar}</p>
-                                                <p>学科: {item.subject}</p>
-                                                <p>描述: {item.content}</p>
-                                                <p>你确定接受此视频任务吗?</p>
-                                            </Modal>
                                         </div>
                                     )
                             })
