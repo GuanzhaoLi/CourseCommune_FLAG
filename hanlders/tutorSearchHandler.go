2b97509f583125d69ad2bda38cec411ff2b835fd
JiaBi <jbi1@mtu.edu>
2021-02-09 23:08:27 -0500

    delete questionSearchHandler.go modify tutorSearch

--- hanlders/tutorSearchHandler.go
+++ hanlders/tutorSearchHandler.go
@@ -1,7 +1,16 @@
 package main
 
-import {
+import (
+    "fmt"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+)
 
+type Schedule struct {
+	subject     string `json:"subject"`
+	startTime 	string `json:"startTime"`
+	endTime		string `json:"endTime"`
+	level     	string `json:"level"`
 }
 
 type Schedule struct {
@@ -16,9 +25,10 @@ type Tutor struct {
 func tutorSearchHandler(w http.ResponseWriter, r *http.Request) {
 	// get the Schedule
 	schedule := Schedule {
-		questionText: r.formValue("questionText"),
-		tutorID: r.formValue("tutorID") // empty here
-		// ... 
+		subject: r.formValue("subject"),
+		startTime: r.formValue("startTime") 
+		endTime: r.formValue("endTime") 
+		level: r.formValue("level")
 	}
 
 	// search for available tutors
@@ -32,8 +42,22 @@ func tutorSearchHandler(w http.ResponseWriter, r *http.Request) {
 
 func findTutors(schedule *Schedule) []Tutor {
 	// create database client
+	db, err = sql.open("mysql", "root:pass1@tcp(127.0.0.1:3306)/Tutor") // need modify dataSourceName
+	                                                                     // name, password, and IP address
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
 	// search for available tutors, based on subject, level, and available table
+	results, err := db.Query("SELECT * FROM Tutor WHERE Level = schedule.level 
+		AND schedule.startTime >= StartTime 
+		AND schedule.endTime <= EndTime") // 命令可能不对
+
+	if err != nil {
+		panic(err.Error())
+	}
 
 	// return a list of Tutors
+	return results // ??
 }
