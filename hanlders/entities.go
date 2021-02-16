4fcfee08d624da76648f85464ee953419a1af5c2
Handsomenick1 <58188159+Handsomenick1@users.noreply.github.com>
2021-02-16 18:17:47 -0500

    final v1.0

--- hanlders/entities.go
+++ hanlders/entities.go
@@ -1,22 +1,40 @@
 package main
 
-import (
-	"time"
-)
+import "time"
 
+type TutorHistory struct {
+	Id      int64 `json:"tutorId"`
+	OrderId int64 `jason:"VideoOrder"`
+	Qid     int64 `json:"QuestionOrder"`
+}
+
+type StudentHistory struct {
+	Id      int64 `json:"studentId"`
+	OrderId int64 `jason:"VideoOrder"`
+	Qid     int64 `json:"QuestionOrder"`
+}
+
+type Users struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+type AnswerQuestionRequestBody struct {
+	questionId int `json:"questionId"`
+	answer string `json:"answer"`
+}
 type VideoOrder struct {
 	OrderId		int64 `json:"orderId"`
 	StartTime	time.Time `json:"startTime"`
 	EndTime		time.Time `json:"endTime"`
 	RequestBy	int64 `json:"requestedBy"`
 	FulfilledBy	int64 `json:"fulfilledBy"`
-	Duration	int64 `json:"duration"`	
+	Duration	int64 `json:"duration"`
 	Subject		string `json:"subject"`
 	Level		int64 `json:"level"`
 	Keywords	string `json:"keywords"`
 	Agreed		int64 `json:"agreed"`
 	T_s_rating	int64 `json:"t_s_rating"`  // tutor gives rate on student
-	S_t_rating	int64 `json:"s_t_rating"`	
+	S_t_rating	int64 `json:"s_t_rating"`
 }
 
 type QuestionOrder struct {
@@ -24,7 +42,7 @@ type QuestionOrder struct {
 	StartTime	time.Time `json:"startTime"`
 	EndTime		time.Time `json:"endTime"`
 	RequestBy	int64 `json:"requestBy"`
-	FulfilledBy	int64 `json:"fulfilledBy"`	
+	FulfilledBy	int64 `json:"fulfilledBy"`
 	Subject		string `json:"subject"`
 	Level		int64 `json:"level"`
 	Keywords	string `json:"keywords"`
@@ -33,23 +51,17 @@ type QuestionOrder struct {
 }
 
 type Criteria struct {
-	Level     int64 
-	Subject   string 
+	Level     int64
+	Subject   string
 }
 type Keywordst struct {
-	Keywords	string
+	Keywords	string `jason:"keywords"`
 }
 type Questionn struct {
-	StudentId int64
-	Level int64
-	Subject string
-	Keywords string
-}
-
-type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	IsTutor bool `json:"isTutor"` //does Go have enums?
+	StudentId 	int64 `jason:"studentId"`
+	Level 		int64	`json:"level"`
+	Subject 	string	`jason:"subject"`
+	Keywords 	string	`json:"keywords"`
 }
 
 type Video struct {
@@ -59,4 +71,49 @@ type Video struct {
 	Subject     string `jason:"Subject"`
 	Description string `jason:"Description"`
 	Fulefilled  string `jason:"Fulefilled"`
+}
+
+type Student struct {
+	Id        string `json:"id"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Level     string `json:"level"`
+	// Rating           string `json:"rating"`
+	// AccountBalance   string `json:"account_balance"`
+	// UserId           string `json:"user_id"`
+	// StudentHistoryId string `json:"student_history_id"`
+	// TutorHistoryId   string `json:"tutor_history_id"`
+	// isTutor bool `json:"isTutor"` //does Go have enums?
+}
+
+type Tutor struct {
+	Id        string `json:"id"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Level     string `json:"level"`
+	Subject   string `json:"subject"`
+	// Account_Balance int,
+	// Rating int,
+}
+type Tutorr struct { // 内部的东西需要开头大写吗。其他api也用了 Tutor struct，但不一样。FY
+	TutorId     int64 `json:"tutorId"`
+	FirstName   string `json:"firstName"`
+	LastName   	string `json:"lastName"`
+	Rating		int64 `json:"rating"`
+}
+
+type PickQuestion struct {
+	QId int64
+	FulfilledBy int64
+	Answer string
+}
+
+type Tcriteria struct {
+	TutorId int64
+	Level int64
+	Subject string
 }
